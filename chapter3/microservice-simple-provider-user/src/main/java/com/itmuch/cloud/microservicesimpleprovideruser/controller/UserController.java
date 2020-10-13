01a9dde1b48f314101711feaa4a965dd8fed7739
e <383362161@qq.com>
2020-10-13 16:29:47 +0800

    createUser

--- chapter3/microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
+++ chapter3/microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/controller/UserController.java
@@ -3,9 +3,7 @@ package com.itmuch.cloud.microservicesimpleprovideruser.controller;
 import com.itmuch.cloud.microservicesimpleprovideruser.dao.UserRepository;
 import com.itmuch.cloud.microservicesimpleprovideruser.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author:crelle
@@ -20,10 +18,15 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/{id}")
+    @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
         User findOne = this.userRepository.getOne(id);
         return findOne;
+    }
+
+    @PostMapping("/user")
+    public User saveUser(User user){
+       return this.userRepository.save(user);
     }
 
 
