620de8142d6ae4c605a1548480705cdbb2003108
e <383362161@qq.com>
2020-10-13 21:23:13 +0800

    test spring boot actuator

--- chapter3/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
+++ chapter3/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.itmuch.cloud.microservicesimpleconsumermovie.controller;
 
 import com.itmuch.cloud.microservicesimpleconsumermovie.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,11 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
-        return this.restTemplate.getForObject("http://localhost:8000/user/"+id,User.class);
+        return this.restTemplate.getForObject(userServiceUrl+id,User.class);
     }
 }
